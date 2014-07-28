83076f96c8daa8a668e497304b131d4a76bec157
Francis Chibaye <francischibaye@yahoo.com>
2014-07-28 06:57:59 +0100

    Updated assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Loudius::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( main.js main.css mobile.js mobile.css ios.js ios.css)
+  #config.assets.precompile += %w( main.js main.css mobile.js mobile.css ios.js ios.css)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
